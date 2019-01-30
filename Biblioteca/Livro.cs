167eba677b0e977a50335174fa7353c3bdb0048d
JefersonPacheco <susulo@hotmail.com>
2019-01-30 16:26:30 -0200

    Inserção do campo "ano"

--- Biblioteca/Livro.cs
+++ Biblioteca/Livro.cs
@@ -10,5 +10,6 @@ namespace Biblioteca
         public string Isbn { get; set; }
         public string Titulo { get; set; }
         public string Autor { get; set; }
+        public string ano { get; set; }
     }
 }
